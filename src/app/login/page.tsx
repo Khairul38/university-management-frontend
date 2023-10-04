f59d11fc9bfc863aa77ac5068466a27dd58abe54
Khairul Alam <khairulislam38@gmail.com>
2023-10-04 12:10:12 +0600

    [Done] login page, reusable form component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,61 @@
+"use client";
+
+import { Button, Col, Row } from "antd";
+import loginImage from "@/assets/login-image.png";
+import Image from "next/image";
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import { SubmitHandler } from "react-hook-form";
+
+type FormValues = {
+  id: string;
+  password: string;
+};
+
 const LoginPage = () => {
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    try {
+      console.log(data);
+    } catch (error) {}
+  };
+
   return (
-    <div>
-      <h1>This is login page</h1>
-    </div>
+    <Row justify="center" align="middle" style={{ minHeight: "100vh" }}>
+      <Col sm={12} md={16} lg={11}>
+        <Image src={loginImage} width={500} height={500} alt="login image" />
+      </Col>
+      <Col sm={12} md={8} lg={8}>
+        <h1
+          style={{
+            margin: "15px 0px",
+          }}
+        >
+          First login your account
+        </h1>
+        <div>
+          <Form submitHandler={onSubmit}>
+            <div>
+              <FormInput label="User Id" name="id" type="text" size="large" />
+            </div>
+            <div
+              style={{
+                margin: "15px 0px",
+              }}
+            >
+              <FormInput
+                label="User Password"
+                name="password"
+                type="password"
+                size="large"
+              />
+            </div>
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form>
+        </div>
+      </Col>
+    </Row>
   );
 };
 
--- src/components/ui/Contents.tsx
+++ src/components/ui/Contents.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { Layout } from "antd";
+import UMBreadCrumb from "./UMBreadCrumb";
 
 const { Content } = Layout;
 
 const Contents = ({ children }: { children: React.ReactNode }) => {
+  const base = "admin";
   return (
     <Content
       style={{
@@ -12,6 +14,18 @@ const Contents = ({ children }: { children: React.ReactNode }) => {
         color: "black",
       }}
     >
+      <UMBreadCrumb
+        items={[
+          {
+            label: `${base}`,
+            link: `/${base}`,
+          },
+          {
+            label: "student",
+            link: `/${base}/student`,
+          },
+        ]}
+      />
       {children}
     </Content>
   );
